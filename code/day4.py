f8da1e2a6d3bd64cd756c95737bdd2e380b16857
jverbeek <janneke.verbeek@rtl.nl>
2022-12-04 09:36:15 +0100

    day 4 prettier

--- code/day4.py
+++ code/day4.py
@@ -5,11 +5,13 @@ with open("inputs/day4.txt") as f:
 def isbetween(n, r1, r2):
     return r1 <= n <= r2
 
-count = 0
+count_contains = 0
+count_overlap = 0
 for line in inp:  #1-5 2-4   2 12  1 10
     a1, a2 = int(line[0][0]), int(line[0][1])
     b1, b2 = int(line[1][0]), int(line[1][1])
     if isbetween(a1, b1, b2) or isbetween(a2, b1, b2) or isbetween(b1, a1, a2) or isbetween(b2, a1, a2):
-        count += 1
-
-print(count)
+        count_overlap += 1
+    if isbetween(a1, b1, b2) and isbetween(a2, b1, b2) or isbetween(b1, a1, a2) and isbetween(b2, a1, a2):
+        count_contains += 1
+print(count_overlap, count_contains)
