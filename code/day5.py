9e08f74d8d0aabbf9aa7ae98f1d4a97353dd1dc7
jverbeek <janneke.verbeek@rtl.nl>
2022-12-05 11:46:34 +0100

    *cries in input parsing*

---
+++ code/day5.py
@@ -0,0 +1,18 @@
+import re
+with open("inputs/day5.txt") as f:
+    inp = f.readlines()
+
+boxes = [[] for i in range(9)]
+
+for line in inp:
+    for i in range(len(line)):
+        if line[i].isupper():
+            letter, xind = line[i], int((i+3)/4)
+            boxes[xind-1].append(letter)
+    m = re.search("\d+ from \d to \d", line)
+    if m:  # beginning of array is top box
+        instruction = m.group(0).split(" ")
+        amount, fr, to = int(instruction[0]), int(instruction[2]), int(instruction[4])
+        boxes[to-1] = boxes[fr-1][:amount] + boxes[to-1]  # take amount boxes from beginning of array, reverse
+        boxes[fr-1] = boxes[fr-1][amount:]
+print("".join([box[0] for box in boxes]))
